1914256d3be80a5a985c87930362e51df64ff106
ppunem <prathyusha20994@gmail.com>
2024-09-27 19:11:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+      return (
+          <div className='bg'>
+            <h1 className='main-head'>Congratulations</h1>
+            <div className='card'>
+             <h1 className='name'>Prathyusha Punem</h1>
+             <p className='college'>
+               Aurora's Technological and Research Institute
+             </p>
+          </div>
+      </div>
+      );
+     }
+     
 export default App;
